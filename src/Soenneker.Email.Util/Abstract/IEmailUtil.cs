9fcc098877abb0142f0c64d5404617636044fda9
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:27 -0500

    xml commenting

--- src/Soenneker.Email.Util/Abstract/IEmailUtil.cs
+++ src/Soenneker.Email.Util/Abstract/IEmailUtil.cs
@@ -9,5 +9,12 @@ namespace Soenneker.Email.Util.Abstract;
 /// </summary>
 public interface IEmailUtil
 {
+    /// <summary>
+    /// Executes the place on queue operation.
+    /// </summary>
+    /// <typeparam name="T">The T type.</typeparam>
+    /// <param name="msgModel">The msg model.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     ValueTask PlaceOnQueue<T>(T msgModel, CancellationToken cancellationToken = default) where T : Message;
 }
